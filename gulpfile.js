6b334432d3338f75c5ca30c58f6efcda7d414e98
Kildyashov <denis.kildiaschov@yandex.ru>
2018-05-20 12:08:02 +0300

    Собирает проект тест

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,7 @@ gulp.task("serve", function() {
   });
 
   gulp.watch("source/less/**/*.less", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/*.html", ["html"]).on("change", server.reload);
 });
 
 gulp.task("copy", function() {
